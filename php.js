d9669b9614e6b97ecc790716df0122f3ce261f24
n8davis <nate@nates-MacBook.local>
2017-11-29 20:23:10 -0800

    more funcs

--- php.js
+++ php.js
@@ -42,6 +42,14 @@ PHP.is_bool  = function(bool){
 PHP.is_int = function(int){
 	return int === +int && isFinite(int) && !(int % 1)
 }
+PHP.get_url_param = function( name, url ) {
+    if (!url) url = location.href;
+    name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+    var regexS = "[\\?&]"+name+"=([^&#]*)";
+    var regex = new RegExp( regexS );
+    var results = regex.exec( url );
+    return results == null ? null : results[1];
+}
 PHP.gettype = function(mixedVar) {
   var isFloat = require('../var/is_float')
   var s = typeof mixedVar
@@ -110,6 +118,22 @@ PHP.object_key_exists = function(key,obj){
     }
   }
   return false;
+}
+PHP.isset =  function isset(){
+  var a = arguments
+  var l = a.length
+  var i = 0
+  var undef
+  if (l === 0) {
+    throw new Error('Empty isset')
+  }
+  while (i !== l) {
+    if (a[i] === undef || a[i] === null) {
+      return false
+    }
+    i++
+  }
+  return true
 }
 PHP.htmlspecialchars_decode = function(string, quoteStyle) { 
   var optTemp = 0
@@ -444,6 +468,178 @@ PHP.echo     = function(){
   var args = Array.prototype.slice.call(arguments)
   return console.log(args.join(' '))
 }
+PHP.strlen = function strlen (string) {
+  var str = string + ''
+  var iniVal = (typeof require !== 'undefined' ? require('../info/ini_get')('unicode.semantics') : undefined) || 'off'
+  if (iniVal === 'off') {
+    return str.length
+  }
+  var i = 0
+  var lgth = 0
+  var getWholeChar = function (str, i) {
+    var code = str.charCodeAt(i)
+    var next = ''
+    var prev = ''
+    if (code >= 0xD800 && code <= 0xDBFF) {
+      if (str.length <= (i + 1)) {
+        throw new Error('High surrogate without following low surrogate')
+      }
+      next = str.charCodeAt(i + 1)
+      if (next < 0xDC00 || next > 0xDFFF) {
+        throw new Error('High surrogate without following low surrogate')
+      }
+      return str.charAt(i) + str.charAt(i + 1)
+    } else if (code >= 0xDC00 && code <= 0xDFFF) {
+      if (i === 0) {
+        throw new Error('Low surrogate without preceding high surrogate')
+      }
+      prev = str.charCodeAt(i - 1)
+      if (prev < 0xD800 || prev > 0xDBFF) {
+        throw new Error('Low surrogate without preceding high surrogate')
+      }
+      return false
+    }
+    return str.charAt(i)
+  }
+  for (i = 0, lgth = 0; i < str.length; i++) {
+    if ((getWholeChar(str, i)) === false) {
+      continue
+    }
+    lgth++
+  }
+  return lgth
+}
+PHP.addslashes = function(str){
+  return (str + '')
+    .replace(/[\\"']/g, '\\$&')
+    .replace(/\u0000/g, '\\0')
+}
+PHP.addcslashes = function (str, charlist) {
+  var target = ''
+  var chrs = []
+  var i = 0
+  var j = 0
+  var c = ''
+  var next = ''
+  var rangeBegin = ''
+  var rangeEnd = ''
+  var chr = ''
+  var begin = 0
+  var end = 0
+  var octalLength = 0
+  var postOctalPos = 0
+  var cca = 0
+  var escHexGrp = []
+  var encoded = ''
+  var percentHex = /%([\dA-Fa-f]+)/g
+  var _pad = function (n, c) {
+    if ((n = n + '').length < c) {
+      return new Array(++c - n.length).join('0') + n
+    }
+    return n
+  }
+  for (i = 0; i < charlist.length; i++) {
+    c = charlist.charAt(i)
+    next = charlist.charAt(i + 1)
+    if (c === '\\' && next && (/\d/).test(next)) {
+      rangeBegin = charlist.slice(i + 1).match(/^\d+/)[0]
+      octalLength = rangeBegin.length
+      postOctalPos = i + octalLength + 1
+      if (charlist.charAt(postOctalPos) + charlist.charAt(postOctalPos + 1) === '..') {
+        begin = rangeBegin.charCodeAt(0)
+        if ((/\\\d/).test(charlist.charAt(postOctalPos + 2) + charlist.charAt(postOctalPos + 3))) {
+          rangeEnd = charlist.slice(postOctalPos + 3).match(/^\d+/)[0]
+          i += 1
+        } else if (charlist.charAt(postOctalPos + 2)) {
+          rangeEnd = charlist.charAt(postOctalPos + 2)
+        } else {
+          throw new Error('Range with no end point')
+        }
+        end = rangeEnd.charCodeAt(0)
+        if (end > begin) {
+          for (j = begin; j <= end; j++) {
+            chrs.push(String.fromCharCode(j))
+          }
+        } else {
+          chrs.push('.', rangeBegin, rangeEnd)
+        }
+        i += rangeEnd.length + 2
+      } else {
+        chr = String.fromCharCode(parseInt(rangeBegin, 8))
+        chrs.push(chr)
+      }
+      i += octalLength
+    } else if (next + charlist.charAt(i + 2) === '..') {
+      rangeBegin = c
+      begin = rangeBegin.charCodeAt(0)
+      if ((/\\\d/).test(charlist.charAt(i + 3) + charlist.charAt(i + 4))) {
+        rangeEnd = charlist.slice(i + 4).match(/^\d+/)[0]
+        i += 1
+      } else if (charlist.charAt(i + 3)) {
+        rangeEnd = charlist.charAt(i + 3)
+      } else {
+        throw new Error('Range with no end point')
+      }
+      end = rangeEnd.charCodeAt(0)
+      if (end > begin) {
+        for (j = begin; j <= end; j++) {
+          chrs.push(String.fromCharCode(j))
+        }
+      } else {
+        chrs.push('.', rangeBegin, rangeEnd)
+      }
+      i += rangeEnd.length + 2
+    } else {
+      chrs.push(c)
+    }
+  }
+  for (i = 0; i < str.length; i++) {
+    c = str.charAt(i)
+    if (chrs.indexOf(c) !== -1) {
+      target += '\\'
+      cca = c.charCodeAt(0)
+      if (cca < 32 || cca > 126) {
+        switch (c) {
+          case '\n':
+            target += 'n'
+            break
+          case '\t':
+            target += 't'
+            break
+          case '\u000D':
+            target += 'r'
+            break
+          case '\u0007':
+            target += 'a'
+            break
+          case '\v':
+            target += 'v'
+            break
+          case '\b':
+            target += 'b'
+            break
+          case '\f':
+            target += 'f'
+            break
+          default:
+            encoded = encodeURIComponent(c)
+            if ((escHexGrp = percentHex.exec(encoded)) !== null) {
+              target += _pad(parseInt(escHexGrp[1], 16).toString(8), 3)
+            }
+            while ((escHexGrp = percentHex.exec(encoded)) !== null) {
+              target += '\\' + _pad(parseInt(escHexGrp[1], 16).toString(8), 3)
+            }
+            break
+        }
+      } else {
+        target += c
+      }
+    } else {
+      target += c
+    }
+  }
+  return target
+}
 PHP.implode  = function(){
   var i = ''
   var retVal = ''
@@ -463,6 +659,21 @@ PHP.implode  = function(){
     return retVal
   }
   return pieces
+}
+PHP.rand = function(min,max){
+  var argc = arguments.length
+  if (argc === 0) {
+    min = 0
+    max = 2147483647
+  } else if (argc === 1) {
+    throw new Error('Warning: rand() expects exactly 2 parameters, 1 given')
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+PHP.strpos = function(haystack, needle, offset) {
+  var i = (haystack + '')
+    .indexOf(needle, (offset || 0))
+  return i === -1 ? false : i
 }
 PHP.str_replace = function(search, replace, subject, countObj) {
   var i = 0
